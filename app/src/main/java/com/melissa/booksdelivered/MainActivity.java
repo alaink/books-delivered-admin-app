1bfbf0bba66ea29da5ea0302444a95cbae84d439
KagajuM <kagajum@gmail.com>
2017-06-02 08:52:43 +0200

    Melissa's first commit

---
+++ app/src/main/java/com/melissa/booksdelivered/MainActivity.java
@@ -0,0 +1,21 @@
+package com.melissa.booksdelivered;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Method called when the Start button is clicked in main activity
+    public void onClick (View v) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/melissa/booksdelivered/SignupActivity.java
@@ -0,0 +1,12 @@
+package com.melissa.booksdelivered;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
